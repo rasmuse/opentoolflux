a78f74eee1d71afb08402abb633766f601b002ed
Rasmus Einarsson <mr@rasmuseinarsson.se>
2022-07-11 18:11:08 +0200

    Use logging to create an info command

--- src/picarrito/cli.py
+++ src/picarrito/cli.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import datetime
+import logging
 import os
 from pathlib import Path
 from typing import List, Mapping
@@ -8,9 +10,12 @@ import click
 import pydantic
 import toml
 
-from . import db
+from . import analyze, database, logging_config
+
+logger = logging.getLogger(__name__)
 
 _DEFAULT_CONFIG_PATH = Path("picarrito.toml")
+_DEFAULT_DB_PATH = Path("picarrito_db.feather")
 
 
 @click.group()
@@ -23,7 +28,10 @@ _DEFAULT_CONFIG_PATH = Path("picarrito.toml")
 )
 def main(ctx: click.Context, config_path: Path):
     ctx.ensure_object(dict)
-    ctx.obj["config"] = Config.from_toml(config_path)
+    conf = Config.from_toml(config_path)
+    ctx.obj["config"] = conf
+
+    logging_config.setup_logging(conf.logging)
 
     work_dir = config_path.parent
     os.chdir(work_dir)
@@ -33,38 +41,71 @@ def main(ctx: click.Context, config_path: Path):
 @click.pass_context
 def import_(ctx: click.Context):
     conf: Config = ctx.obj["config"]
-    new_data = db.read_src_files(
+    new_data = database.read_src_files(
         conf.import_.src,
         conf.import_.columns,
         conf.import_.timestamp_col,
         conf.import_.sep,
     )
     try:
-        old_db = db.read_db(conf.general.db_path)
-        updated_db = db.update(old_db, new_data)
+        old_db = database.read_db(conf.general.db_path)
+        updated_db = database.update(old_db, new_data)
     except FileNotFoundError:
         updated_db = new_data
 
-    db.save_db(updated_db, conf.general.db_path)
+    database.save_db(updated_db, conf.general.db_path)
+
+
+@main.command()
+@click.pass_context
+def info(ctx: click.Context):
+    conf: Config = ctx.obj["config"]
+    for _ in _iter_measurements(conf):
+        pass
+
+
+def _iter_measurements(conf: Config):
+    db = database.read_db(conf.general.db_path)
+    db = analyze.filter_db(db, conf.filters)
+    yield from analyze.iter_measurements(
+        db,
+        conf.measurements.chamber_col,
+        conf.measurements.max_gap,
+        conf.measurements.min_duration,
+        conf.measurements.max_duration,
+    )
 
 
 class General(pydantic.BaseModel):
-    db_path: Path
+    db_path: Path = _DEFAULT_DB_PATH
 
 
 class Import(pydantic.BaseModel):
     src: List[str]
     timestamp_col: str
     sep: str = r"\s"
-    columns: Mapping[db.Colname, db.DTypeName]
+    columns: Mapping[database.Colname, database.DTypeName]
+
+
+class Measurements(pydantic.BaseModel):
+    chamber_col: str
+    max_gap: datetime.timedelta
+    min_duration: datetime.timedelta
+    max_duration: datetime.timedelta
 
 
 class Config(pydantic.BaseModel):
     general: General
     import_: Import = pydantic.Field(alias="import")
+    filters: Mapping[database.Colname, analyze.Filter] = pydantic.Field(
+        default_factory=dict
+    )
+    measurements: Measurements
+    logging: Mapping = logging_config.DEFAULT_LOG_SETTINGS
 
     @classmethod
     def from_toml(cls, path: Path) -> Config:
+        logger.debug(f"Reading config file {path}")
         with open(path, "r") as f:
             obj = toml.load(f)
         return cls.parse_obj(obj)
---
+++ src/picarrito/logging_config.py
@@ -0,0 +1,67 @@
+# https://docs.python.org/3/library/logging.config.html#configuration-dictionary-schema
+import logging.config
+import os
+from pathlib import Path
+from typing import Any, Dict, Optional
+
+LogSettingsDict = Dict[str, Any]
+
+DEFAULT_LOG_SETTINGS = {
+    "formatters": {
+        "detailed": {
+            "format": "%(asctime)s %(levelname)-8s %(name)-22s %(message)s",
+        },
+        "brief": {
+            "format": "%(levelname)-8s %(message)s",
+        },
+    },
+    "filters": {
+        "allow_picarrito": {"name": "picarrito"},
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "level": "INFO",
+            "formatter": "brief",
+            "filters": ["allow_picarrito"],
+        },
+        "file_debug": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "level": "DEBUG",
+            "filename": "log.debug.txt",
+            "formatter": "detailed",
+            "maxBytes": 1e6,
+            "backupCount": 5,
+            "filters": ["allow_picarrito"],
+        },
+        "file_info": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "level": "INFO",
+            "filename": "log.info.txt",
+            "formatter": "detailed",
+            "maxBytes": 1e6,
+            "backupCount": 5,
+            "filters": ["allow_picarrito"],
+        },
+    },
+    "root": {
+        "level": "DEBUG",
+        "handlers": [
+            "console",
+            "file_debug",
+            "file_info",
+        ],
+    },
+    "version": 1,
+    "disable_existing_loggers": False,
+}
+
+
+def setup_logging(settings: LogSettingsDict, logging_dir: Optional[Path] = None):
+    if logging_dir is None:
+        logging_dir = Path.cwd()
+    cwd = Path.cwd()
+    logging_dir.mkdir(parents=True, exist_ok=True)
+    os.chdir(logging_dir)
+    logging.config.dictConfig(settings)
+    os.chdir(cwd)
